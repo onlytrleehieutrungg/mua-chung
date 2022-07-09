087bc93b33fd7e523ab71939ea6e50dd99a5b8aa
Smjle <denwingsa01884@gmail.com>
2022-07-09 14:34:07 +0700

    Styling Product Layout

--- src/components/section-title/index.js
+++ src/components/section-title/index.js
@@ -16,13 +16,3 @@ Component({
   },
 });
 
-module.exports = {
-  theme: {
-    textColor: theme => theme('colors'),
-    textColor: {
-      'primary': '#3490dc',
-      'secondary': '#ffed4a',
-       'danger': '#e3342f',
-     }
-  }
-}
